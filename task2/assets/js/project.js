b23b7488bc0d5361564e1801c88d31a1bf90e466
boboiboyss <boysimbolon5@gmail.com>
2024-04-14 14:25:05 +0700

    update file project

--- task2/assets/js/project.js
+++ task2/assets/js/project.js
@@ -1,17 +1,21 @@
+//membuat array kosong (addProject)
 var addProject = []
 
+//membuat function handleProject dengan parameter event
 function handleProject(event) {
-
+    //menghandle default event reload saat function dijalankan / button di klik
     event.preventDefault();
+
+    //mengambil value dari inputan form html
     let projectName = document.getElementById('lproject-name').value;
     let startDate = document.getElementById('lstart-date').value;
     let endDate = document.getElementById('lend-date').value;
     let description = document.getElementById('ldescription').value;
-    // let tech = document.querySelectorAll('input[type="checkbox"]');
     let check = document.querySelectorAll('input:checked');
     let image = document.getElementById('lupload').files[0];
 
 
+    //membuat kondisi validasi / mengecek jika data yang di input bernilai kosong
     if(projectName === "" || startDate === "" || endDate === "" || description === "" || !image) {
         return alert("Please complete the empty fields");
     } else if (startDate > endDate) {
@@ -20,8 +24,11 @@ function handleProject(event) {
          return alert('Please checked one of the box')
     } 
     else {
+
+        //menyimpan url image saat file image di upload
         let urlImage = URL.createObjectURL(image)      
-        let listTech = []
+
+        
         // for (let i = 0; i < tech.length; i++) {
         //    if (tech[i].checked === true) {
         //        listTech.push (tech[i].value);
@@ -32,25 +39,42 @@ function handleProject(event) {
         //    } 
         // }
 
+        //membuat array kosong untuk menampung nilai / item checkbox saat di klik
+        let listTech = []
+        
+        //menampung nilai item checkbox
         check.forEach(item => {
             listTech.push(item.value);
         })
 
+        //mengubah value date menjadi array 
         let startDatePart = startDate.split('/');
         let endDatePart = endDate.split('/');
 
+        //membuat format date / custom date sesuai region indonesia
         let formatStartDate = `${startDatePart[2]} - ${startDatePart[1]} - ${startDatePart[0]}`;
         let formatEndDate = `${endDatePart[2]} - ${endDatePart[1]} - ${endDatePart[0]}`
 
+        //membuat date baru sesuai format region indonesia
         let newStartDate = new Date(formatStartDate);
         let newEndDate = new Date(formatEndDate);
 
+        //menghitung perbedaan waktu date dalam milisecond/
         let timeDifference = newEndDate - newStartDate
+        
+        //konversi jumlah hari date, konversi milisecond ke hari
         let getDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        //konversi jumlah bulan, jumlah hari date dibagi dengan nilai rata" hari dalam 1 bulan
         let getMonths = Math.floor(getDays / 30.44);
+
+        //konversi jumlah tahun, jumlah bulan date dibagi dengan jumlah bulan dalam 1 tahun
         let getYears =  Math.floor(getMonths / 12)
         
+        //membuat variable duration
         let duration;
+
+        //membuat kondisi validasi / mengecek waktu date dan menampung nilai waktu date
         if(getYears > 0) {
             duration = `${getYears} Tahun`
         } else if (getMonths > 0) {
@@ -59,6 +83,7 @@ function handleProject(event) {
             duration = `${getDays} Hari`
         }
 
+        //menampung semua nilai inputan kedalam addProject
         addProject.push({
             title : projectName,
             startDate,
@@ -70,9 +95,13 @@ function handleProject(event) {
         })
     }
 
+    //menyimpan semua data addProject kedalam localStorage
     localStorage.setItem('myProject', JSON.stringify(addProject));
     
+    //mengambil element html dengan id card-project
    const temp = document.getElementById('card-project');
+
+   //menambahkan element html kedalam temp dan menampilkan semua data / item dari addProject
    addProject.map((item, key) => {
         temp.innerHTML += `<div class='card-content'>
                 <img src='${item.image}' alt='img'/>
--- task2/assets/js/projectDetail.js
+++ task2/assets/js/projectDetail.js
@@ -1,10 +1,17 @@
+//membuat function addEventListener dengan event DOMContentLoaded, agar halaman html siap digunakan 
 document.addEventListener('DOMContentLoaded', function() {
 
+//mengambil data myProject dari localstorage
 let storeProject = JSON.parse(localStorage.getItem('myProject'))
+
+//mengambil params id dari location search dan mengambil data myProject sesuai params id
 const id = location.search.substring(4);
 const detailProject = storeProject[id-1];
+
+//mendestruction detailProject dan membuat variable baru sesuai properti detailProject
 const {title, startDate, endDate, description, image, duration, listTech} = detailProject;
 
+//membuat format startDate dan conversi bulan menjadi nama bulan / string
 const newStartDate = new Date(startDate);
 const yearStartDate = newStartDate.getFullYear();
 const monthStartDate = newStartDate.getMonth();
@@ -12,6 +19,7 @@ const converMonthStart = newStartDate.toLocaleString('en-US', {month: 'long'});
 const dateStartDate = String(newStartDate.getDate()).padStart(2, "0"); 
 const formatStartDate = `${dateStartDate} ${converMonthStart.substring(0,3)} ${yearStartDate}`;
 
+//membuat format endDate dan konversi bulan menjadi nama bulan / string
 const newEndDate = new Date(endDate);
 const yearEndDate = newEndDate.getFullYear();
 const monthEndDate = newEndDate.getMonth();
@@ -19,15 +27,22 @@ const convertMonthEnd = newEndDate.toLocaleString('en-US', {month: 'long'});
 const dateEndDate = String(newEndDate.getDate()).padStart(2, "0");
 const formatEndDate = `${dateEndDate} ${convertMonthEnd.substring(0,3)} ${yearEndDate}`
 
+// konversi item array listTech menjadi string dan menggabung semua item menjadi satu string
 let tech = listTech.map(item => item.replace(",", "")).join(" ");
+
+//konversi string tech menjadi array dengan memotong character "Js" dan membuat array baru
 let icons = tech.split("Js");
+
+//menggunakan regex untuk mencari spasi pada item array icons dan mengganti menjadi string kosong
 let icon = icons.map(item => item.replace(/\s/g, ""));
 
+//membuat varible string kosong untuk menampung nilai item icon
 let react = '';
 let node = '';
 let ts = '';
 let next = '';
 
+//membuat perulangan pada array icon, membuat kondisi pengecekan nial tiap item dan menampung nilai item icon
 for(let i = 0; i < icon.length; i++){
      if(icon[i] === "React") {
         react += `<div><i class="fa-brands fa-${icon[i].toLowerCase()}" size="20"></i> <span>${icon[i].concat(" ", "Js")}</span></div>`
@@ -40,7 +55,10 @@ for(let i = 0; i < icon.length; i++){
      }
 }
 
+//mengambil element html dengan id project
 let temp = document.getElementById('project');
+
+//menambahkan element html kedalam temp dan menampilkan semua data / item dari myProject
 temp.innerHTML += `<div class="container-detail">
     <h1>${title}</h1>
     <div class="wrapper">
